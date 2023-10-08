2143d7271287e640155cf76103b36c1b0ffb4d51
JScarry <98224493+JScarry@users.noreply.github.com>
2023-10-08 08:59:59 +0100

    library work

--- library.py
+++ library.py
@@ -12,4 +12,7 @@ for bookNode in BookNodeList:
     print("->")
     titleNode = bookNode.getElementsByTagName("title").item(0)
     title = titleNode.firstChild.nodeValue.strip()
-    print (title)
+    print ("Title: ", title)
+    authorNode = bookNode.getElementsByTagName("author").item(0)
+    author = authorNode.firstChild.nodeValue.strip()
+    print ("Author: ", author)
