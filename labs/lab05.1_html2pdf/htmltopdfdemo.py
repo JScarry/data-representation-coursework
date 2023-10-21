a108accee7bda3e539eed64a9a272da1fb9056b3
JScarry <98224493+JScarry@users.noreply.github.com>
2023-10-21 23:01:16 +0100

    html2pdf lab

---
+++ labs/lab05.1_html2pdf/htmltopdfdemo.py
@@ -0,0 +1,23 @@
+import requests
+import urllib.parse
+from config import config as cfg
+
+targetUrl = "https://en.wikipedia.org/wiki/API_key"
+
+apiKey = cfg["htmltopdfkey"]
+
+apiurl = 'https://api.html2pdf.app/v1/generate'
+
+params = {'url': targetUrl,'apiKey': apiKey}
+parsedparams = urllib.parse.urlencode(params)
+
+requestUrl = apiurl +"?" + parsedparams 
+print (requestUrl)
+
+response = requests.get(requestUrl)
+
+print (response.status_code)
+result =response.content
+
+with open("document.pdf", "wb") as handler:
+    handler.write(result)
